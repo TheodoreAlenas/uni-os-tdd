ade3deb5daa095f4fc4d507504f90d0be3f00471
Theodore Alenas <dimakopt732@gmail.com>
2022-12-03 13:21:39 +0200

    better testing environment

--- c/tests/test-main.c
+++ c/tests/test-main.c
@@ -6,23 +6,25 @@ void test_one(bool (*f) (void), char *name);
 
 int main() {
 
-  test_stack_is_empty();
-  //test_one(test_stack_is_empty, "stack is empty");
+  test_one(test_stack_is_empty, "stack is empty");
 
   return 0;
 }
 
 void test_one(bool (*f) (void), char *name) {
   bool passed;
+  char *red   = "\033[0;31m";
+  char *green = "\033[0;32m";
+  char *no_color = "\033[0m";
 
-  printf("Testing function '%s' ... ", name);
+  printf("%sTesting function '%s' ... %s", green, name, no_color);
   passed = f();
 
   if (passed) {
-    printf("Passed");
+    printf("%sPassed%s\n", green, no_color);
   }
   else {
-    printf("Failed");
+    printf("%sFailed%s\n", red, no_color);
   }
 
 }
