17dd51fcce2ed568abb7f84af7d4925de549a10c
Theodore Alenas <dimakopt732@gmail.com>
2022-12-04 13:47:01 +0200

    parent elsewhere

--- c/src/manager.c
+++ c/src/manager.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-#include "stack.h"
-#include "child_data.h"
+#include "parent.h"
 
 
-
-typedef struct main_resources {
-  Stack *requests;
-  ChildData *children;
-} MainResources;
-
-MainResources *create_resources_of_main(int num_of_workers);
-int main_loop(MainResources *r);
-void free_resources_of_main(MainResources *r);
 
 int main() {
   int num_of_workers = 10;
@@ -23,27 +13,5 @@ int main() {
   free_resources_of_main(r);
 
   return main_ret;
-}
-
-
-MainResources *create_resources_of_main(int num_of_workers) {
-  MainResources *r = malloc(sizeof(MainResources));
-  r->children = child_data_create(num_of_workers);
-  r->requests = stack_create(num_of_workers);
-  return r;
-}
-
-void free_resources_of_main(MainResources *r) {
-  stack_free(r->requests);
-  child_data_free(r->children);
-  free(r);
-}
-
-int main_loop(MainResources *r) {
-  printf("hello\n");
-#ifdef DEV
-  printf("dev is defined\n");
-#endif
-  return 0;
 }
 
