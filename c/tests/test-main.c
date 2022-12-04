89dda42f51e1dbc758d15d3a0a3477b13a30b40a
Theodore Alenas <dimakopt732@gmail.com>
2022-12-04 21:07:13 +0200

    refactor on test-main.c

--- c/tests/test-main.c
+++ c/tests/test-main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "test.h"
 
-int main() {
+void call_all();
+void call_individual_with_same_name(char *name);
+void say_final_results();
 
+
+int main(int argc, char **argv) {
+  int i;
+
+  if (argc == 1)
+    call_all();
+
+  for (i = 1; i < argc; i++)
+    call_individual_with_same_name(argv[i]);
+
+  say_final_results();
+  return get_total_tests_failed();
+}
+
+void call_all() {
   test_stack();
   test_arg_parse();
+}
 
+#define CONSIDER(n) else if (strcmp(name, #n) == 0) test_##n()
+
+void call_individual_with_same_name(char *name) {
+  if (0) return;
+  CONSIDER(stack);
+  CONSIDER(arg_parse);
+}
+#undef CONSIDER
+
+void say_final_results() {
   printf("%d/%d passed, %d failed\n",
       get_total_tests_passed(),
       get_total_tests(),
       get_total_tests_failed());
-
-  return get_total_tests_failed();
 }
-
