cdcbd6662949b062e580f4f252d3897a98ae69de
Theodore Alenas <dimakopt732@gmail.com>
2022-12-04 13:35:33 +0200

    child data are a thing and can be created

--- c/src/manager.c
+++ c/src/manager.c
@@ -3,23 +3,24 @@
 #include "stack.h"
 
 
-typedef struct per_child {
+typedef struct child_data {
   int semaphore;
   void *shmem;
   bool is_working;
   int current_file_segment;
-} PerChild;
+} ChildData;
 
-int child_data_create(int num_of_workers);
+ChildData *child_data_create(int num_of_workers);
+void child_data_free(ChildData *c);
 
 typedef struct main_resources {
   Stack *requests;
-  PerChild *children;
+  ChildData *children;
 } MainResources;
 
 MainResources *create_resources_of_main(int num_of_workers);
 int main_loop(MainResources *r);
-int free_resources_of_main(MainResources *r);
+void free_resources_of_main(MainResources *r);
 
 int do_the_rest(int num_of_workers);
 
@@ -30,13 +31,9 @@ int main() {
 
   MainResources *r = create_resources_of_main(num_of_workers);
   main_ret = main_loop(r);
-  free_ret = free_resources_of_main(r);
+  free_resources_of_main(r);
 
-  if (main_ret != 0)
-    return main_ret;
-  if (free_ret != 0)
-    return free_ret;
-  return 0;
+  return main_ret;
 }
 
 
@@ -46,10 +43,32 @@ int do_the_rest(int num_of_workers) {
 
 
 MainResources *create_resources_of_main(int num_of_workers) {
-  PerChild *children = malloc(num_of_workers * sizeof(PerChild));
-  Stack *requests = stack_create(num_of_workers);
-  MainResources *r = malloc(num_of_workers * sizeof(MainResources));
+  MainResources *r = malloc(sizeof(MainResources));
+  r->children = child_data_create(num_of_workers);
+  r->requests = stack_create(num_of_workers);
   return r;
+}
+
+ChildData *child_data_create(int num_of_workers) {
+  /* TODO malloc fail case */
+  ChildData * c = malloc(num_of_workers * sizeof(ChildData));
+  for (int i = 0; i < num_of_workers; i++) {
+    c[i].is_working = false;
+    c[i].semaphore = 0;
+    c[i].shmem = NULL;  /* TODO create segment */
+    c[i].current_file_segment = 0;
+  }
+  return c;
+}
+
+void child_data_free(ChildData *c) {
+  free(c);  /* TODO clear memory */
+}
+
+void free_resources_of_main(MainResources *r) {
+  stack_free(r->requests);
+  child_data_free(r->children);
+  free(r);
 }
 
 int main_loop(MainResources *r) {
@@ -60,6 +79,3 @@ int main_loop(MainResources *r) {
   return 0;
 }
 
-int free_resources_of_main(MainResources *r) {
-  return 0;
-}
