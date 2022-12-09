af5884556ea2465c1bf763618803b68694763ace
Theodore Alenas <dimakopt732@gmail.com>
2022-12-09 11:41:06 +0200

    the fork loop calls a separate function

--- c/src/fork.c
+++ c/src/fork.c
@@ -16,11 +16,10 @@
 
 int be_parent(Params *p, void *shmem);
 int be_child(int child_index, pid_t *set_me, pid_t to_this);
+int give_birth(unsigned child_index, ChildData *children, char *output_dir);
 
 int handle_forks(Params *p, void *shmem) {
-  pid_t pid, is_parent;
-  int child_index, num_of_children;
-  char *output_file_name, *childs_sem_name;
+  unsigned child_index, num_of_children;
   ChildData *children;
 
   num_of_children = p->parent_params->num_of_children;  /* alias */
@@ -29,25 +28,36 @@ int handle_forks(Params *p, void *shmem) {
   if (children == NULL)
     return -1;
 
-  for (child_index = 0; child_index < num_of_children; child_index++) {
-    output_file_name = get_output_file_name(p->output_dir, child_index);
+  for (child_index = 0; child_index < num_of_children; child_index++)
+    if (give_birth(child_index, children, p->output_dir) != 0)
+      return -1;
 
-    pid = is_parent = fork();
-
-    if (is_parent == -1) {
-      perror("Fork failed: ");
-      return 1;
-    }
-
-    if (is_parent)
-      child_data_create(children + child_index, get_semaphore_name(child_index));
-    else
-      return be_child(child_index, &children[child_index].pid, pid);
-    /* TODO free the output_file name */
-  }
   WELL("forks done");
   p->parent_params->children = children;
   return be_parent(p, shmem);
+}
+
+int give_birth(unsigned child_index, ChildData *children, char *output_dir) {
+
+  pid_t pid, is_parent;
+  char *output_file_name, *childs_sem_name;
+
+  output_file_name = get_output_file_name(output_dir, child_index);
+
+  pid = is_parent = fork();
+
+  if (pid == -1) {
+    perror("Fork failed: ");
+    return 1;
+  }
+
+  if (is_parent)
+    child_data_create(children + child_index, get_semaphore_name(child_index));
+  else
+    return be_child(child_index, &children[child_index].pid, pid);
+  /* TODO free the output_file name */
+
+  return 0;
 }
 
 int be_parent(Params *p, void *shmem) {
@@ -85,3 +95,4 @@ int be_child(int child_index, pid_t *set_me, pid_t to_this) {
 
   return 0;
 }
+
