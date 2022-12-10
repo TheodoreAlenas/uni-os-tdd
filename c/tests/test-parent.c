fa41c770f823ca0c56de9d143dc268ec6ec88197
Theodore Alenas <dimakopt732@gmail.com>
2022-12-10 11:05:41 +0200

    crap I can't test the parent as is

--- c/tests/test-parent.c
+++ c/tests/test-parent.c
@@ -1,30 +1,40 @@
 #include "../src/parent.h"
 #include "test.h"
 
-static int num_of_wait;
-static int num_of_post;
+enum Action { A_READ, A_WRITE, A_POST, A_WAIT };
+static int head;
+static enum Action actions[256];
 
 char *parent_read_file_segment(const Parent *parent, unsigned long segment) {
-  char *res = malloc(64);
+  char *res;
+  res = malloc(64);
   strcpy(res, "hi there\nhow are you\n");
   return res;
 }
 
 int testable_wait(sem_t *sem) {
-
+  actions[head++] = A_WAIT;
+  return 0;
 }
 
 int testable_post(sem_t *sem) {
+  actions[head++] = A_POST;
+  return 0;
+}
 
+int testable_sprintf(void *shm, char *str1, char *str2) {
+  actions[head++] = A_WRITE;
+  return 0;
+}
+
+char *testable_shm_read(void *shm) {
+  actions[head++] = A_READ;
+  return 0;
 }
 
 void test_parent() {
-  Parent *r;
-  ParentParams *pp;
-  announce("test_parent_exists", true);
-  pp = parent_params_create();
-  r = parent_create(NULL);
-  parent_loop(r);
-  parent_free(r);
+
+  announce("test_parent_exists_but_cant_do_much", true);
+  //parent_loop(NULL);
 }
 
