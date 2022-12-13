00521531bfe5411b1a6ce48d3c0dc645300c92f9
Theodore Alenas <dimakopt732@gmail.com>
2022-12-13 19:26:39 +0200

    msg_cycler does the basics

--- c/src/msg_cycler.c
+++ c/src/msg_cycler.c
@@ -3,29 +3,23 @@
 #include "msg_cycler.h"
 #include "constants.h"
 
-    /*
+#define CYCLE_HEAD if (++cycler->head >= cycler->size) cycler->head = 0
+
 char *msg_cycler_find(MsgCycler *cycler) {
   int i;
   char *msg_spot;
-  for (i = 0; i < cycler->size; i++) {
 
-    WELLL(printf("loop %d/%d", cycler->head, cycler->size));
-    cycler->head++;
-    if (cycler->head >= cycler->size) {
-      cycler->head = 0;
-    WELL("full circle");
-    }
+  CYCLE_HEAD;
+
+  for (i = 0; i < cycler->size; i++) {
 
     msg_spot = cycler->messages + cycler->head * MAX_LINE_LEN;
     if ('\0' != *msg_spot)
       return msg_spot;
 
-    return cycler->messages;
+    CYCLE_HEAD;
   }
   return NULL;
 }
-      */
 
-char *msg_cycler_find(MsgCycler *cycler) {
-  return cycler->messages;
-}
+#undef CYCLE_HEAD
--- c/tests/test-msg-cycler.c
+++ c/tests/test-msg-cycler.c
@@ -6,7 +6,7 @@ void test_msg_cycler() {
   MsgCycler mc;
   mc.head = 0;
   mc.size = 1;
-  mc.messages = malloc(MAX_LINE_LEN * 1);
+  mc.messages = malloc(MAX_LINE_LEN * 10);
   sprintf(mc.messages, "a");
   announce("finds_one", 0 == strcmp("a", msg_cycler_find(&mc)));
 
@@ -14,5 +14,15 @@ void test_msg_cycler() {
   mc.size = 1;
   sprintf(mc.messages, "");
   announce("finds_none", NULL == msg_cycler_find(&mc));
+
+  mc.head = 1;
+  mc.size = 2;
+  sprintf(mc.messages, "a");
+  announce("loops_back", 0 == strcmp("a", msg_cycler_find(&mc)));
+
+  mc.messages[MAX_LINE_LEN] = 'b';
+  mc.messages[MAX_LINE_LEN + 1] = '\0';
+  announce("continues", 0 == strcmp("b", msg_cycler_find(&mc)));
+
 }
 
