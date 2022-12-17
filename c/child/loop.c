f3d7b2425ee3eeb16f78959ef97606a65a7d5598
Theodore Alenas <dimakopt732@gmail.com>
2022-12-17 21:05:02 +0200

    all child times, plus prettier loop

--- c/child/loop.c
+++ c/child/loop.c
@@ -22,6 +22,13 @@ int child_loop_backend(const Child *child) {
 }
 
 typedef struct { int file_segment; int line_in_segment; } SegmAndLine;
+typedef struct {
+    struct timespec req_time;
+    struct timespec res_start;
+    struct timespec res_end;
+} ThreeTimespecs;
+void post_and_wait(const Child *child, ThreeTimespecs * t);
+
 
 SegmAndLine write_a_request(const Child *child) {
   char req_str[MAX_REQUEST_LEN];
@@ -49,17 +56,40 @@ void print_isolate_line_error(const Child *child, SegmAndLine d) {
         ((char *) child->shmem_thank_you)[1]);
 }
 
-void record(char *content, const Child *child, SegmAndLine d, int time1, int time2) {
+void record(const Child *child, ThreeTimespecs *t, SegmAndLine d, char *content) {
   ChildRes res;
 
   res.file_segment = d.file_segment;
   res.line_in_segment = d.line_in_segment;
-  res.application_time_in_ns = time1;
-  res.responce_time_in_ns = time2;
+  res.application_time_in_ns = t->req_time.tv_nsec;
+  res.responce_time_in_ns = t->res_end.tv_nsec - t->res_start.tv_nsec;
   strcpy(res.line_contents, content);
 
   child_res_to_file(&res, child->names->file_name);
 
+}
+
+void post_and_wait(const Child *child, ThreeTimespecs * t) {
+
+  clock_gettime(CLOCK_MONOTONIC, &t->res_start);
+  t->req_time.tv_nsec = t->res_start.tv_nsec - t->res_end.tv_nsec;
+
+  sem_post(child->sem_i_want);
+  sem_wait(child->sem_thank_you);
+
+  clock_gettime(CLOCK_MONOTONIC, &t->res_end);
+}
+
+void tell_you_got_the_message(const Child *child) {
+  WELL("sending 'got the message'");
+  req_send_done(child->shmem_i_want);
+  sem_post(child->sem_i_want);
+}
+
+void record_and_wait(const Child *child, ThreeTimespecs *t, SegmAndLine d, char *content) {
+  record(child, t, d, content);
+  usleep(child->names->microsecond_delay);
+  sem_wait(child->sem_thank_you);
 }
 
 /* for README, do-a-cycle */
@@ -67,29 +97,20 @@ void do_a_cycle(const Child *child) {
   char isolated_line[MAX_LINE_LEN];
   int err, i;
   SegmAndLine d;
-  struct timespec req_start, req_end, res_start, res_end;
+  ThreeTimespecs time_data;
 
   d = write_a_request(child);
+  post_and_wait(child, &time_data);
+  err = !isolate_line(
+      isolated_line,
+      child->shmem_thank_you,
+      d.line_in_segment);
+  tell_you_got_the_message(child);
 
-  clock_gettime(CLOCK_MONOTONIC, &res_start);
-  sem_post(child->sem_i_want);
-  sem_wait(child->sem_thank_you);
-  clock_gettime(CLOCK_MONOTONIC, &res_end);
-
-  err = !isolate_line(isolated_line, child->shmem_thank_you, d.line_in_segment);
-
-  WELL("sending 'got the message'");
-  req_send_done(child->shmem_i_want);
-  sem_post(child->sem_i_want);
-
-  if (err) {
+  if (err)
     print_isolate_line_error(child, d);
-    return;
-  }
-  record(isolated_line, child, d, 3, res_end.tv_nsec - res_start.tv_nsec);
-  usleep(child->names->microsecond_delay);
-
-  sem_wait(child->sem_thank_you);
+  else
+    record_and_wait(child, &time_data, d, isolated_line);
 }
 /* end of snippet */
 
